6ddd9dba3d2f927e60f61325b58a5ac52be4156a
aifayunos <hayfayunos41@gmail.com>
2025-09-03 09:00:10 +0800

    CalculatorForm!

--- CalculatorApplication/Form1.Designer.cs
+++ CalculatorApplication/Form1.Designer.cs
@@ -33,8 +33,9 @@
             this.cbOperator = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtBoxInput2 = new System.Windows.Forms.TextBox();
-            this.lblDisplayTotal = new System.Windows.Forms.Label();
             this.btnEqual = new System.Windows.Forms.Button();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // txtBoxInput1
@@ -60,7 +61,7 @@
             // 
             this.cbOperator.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbOperator.FormattingEnabled = true;
-            this.cbOperator.Location = new System.Drawing.Point(368, 110);
+            this.cbOperator.Location = new System.Drawing.Point(354, 133);
             this.cbOperator.Name = "cbOperator";
             this.cbOperator.Size = new System.Drawing.Size(121, 36);
             this.cbOperator.TabIndex = 2;
@@ -69,7 +70,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(72, 189);
+            this.label3.Location = new System.Drawing.Point(53, 204);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(321, 29);
             this.label3.TabIndex = 4;
@@ -78,26 +79,16 @@
             // txtBoxInput2
             // 
             this.txtBoxInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxInput2.Location = new System.Drawing.Point(399, 179);
+            this.txtBoxInput2.Location = new System.Drawing.Point(399, 201);
             this.txtBoxInput2.Multiline = true;
             this.txtBoxInput2.Name = "txtBoxInput2";
             this.txtBoxInput2.Size = new System.Drawing.Size(144, 39);
             this.txtBoxInput2.TabIndex = 5;
             // 
-            // lblDisplayTotal
-            // 
-            this.lblDisplayTotal.AutoSize = true;
-            this.lblDisplayTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDisplayTotal.Location = new System.Drawing.Point(378, 244);
-            this.lblDisplayTotal.Name = "lblDisplayTotal";
-            this.lblDisplayTotal.Size = new System.Drawing.Size(84, 25);
-            this.lblDisplayTotal.TabIndex = 6;
-            this.lblDisplayTotal.Text = "Answer";
-            // 
             // btnEqual
             // 
             this.btnEqual.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEqual.Location = new System.Drawing.Point(354, 293);
+            this.btnEqual.Location = new System.Drawing.Point(354, 338);
             this.btnEqual.Name = "btnEqual";
             this.btnEqual.Size = new System.Drawing.Size(144, 39);
             this.btnEqual.TabIndex = 7;
@@ -105,12 +96,31 @@
             this.btnEqual.UseVisualStyleBackColor = true;
             this.btnEqual.Click += new System.EventHandler(this.button1_Click);
             // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayTotal.Location = new System.Drawing.Point(269, 272);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(199, 25);
+            this.lblDisplayTotal.TabIndex = 6;
+            this.lblDisplayTotal.Text = "Answer:                  ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(399, 272);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 20);
+            this.linkLabel1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.btnEqual);
             this.Controls.Add(this.lblDisplayTotal);
             this.Controls.Add(this.txtBoxInput2);
@@ -132,9 +142,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtBoxInput2;
-        private System.Windows.Forms.Label lblDisplayTotal;
         private System.Windows.Forms.Button btnEqual;
         private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Label lblDisplayTotal;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
